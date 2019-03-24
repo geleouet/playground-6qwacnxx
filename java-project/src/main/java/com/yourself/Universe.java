e7db3e490d0a955dc1d44b6e45332acb98b134af
ELEOUET Gaëtan <gaetan.eleouet@gmail.com>
2019-03-24 12:38:47 +0100

    ca marche enfin!

--- java-project/src/main/java/com/yourself/Universe.java
+++ java-project/src/main/java/com/yourself/Universe.java
@@ -1,12 +1,26 @@
 // { autofold
 package com.yourself;
 
-import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Universe {
 // }
 
 public static int countAllStars(int... galaxies) {
+     File logFile = new File("datas.json");
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(logFile))) 
+        {
+            
+            bw.write("[[1,12],[2,10],[3,7],[4,4],[5,9],[6,13]]");
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
 	int totalStars = 0;
 	for(int stars : galaxies) {
 		totalStars = stars; // fix me!
